2d03d2a7ff05a1ffcd2977846da4a9d647b8a5e3
WebMats <lorlando127@gmail.com>
2019-02-13 16:06:20 -0500

    finished setting up user routes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code away!
+const express = require('express');
+const app = express();
+
+const userRoutes = require('./routes/user');
+const postsRoutes = require('./routes/posts');
+
+app.use(express.json())
+
+app.use('/user', userRoutes);
+app.use('/posts', postsRoutes);
+
+
+
+app.listen(8000, () => {console.log('Listening on port 8000...')})
+
---
+++ routes/user.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const userDB = require('../data/helpers/userDb');
+
+
+router.post('', (req, res, next) => {
+    const { name } = req.body;
+    if (!name) {
+        res.status(404).json({errorMessage: "Please provide a name field."})
+    } else {
+        userDB.insert({name}).then((result) => {
+            if (!result) {
+                res.status(404).json({errorMessage: "Could not create that user."})
+            } else {
+                res.status(201).json(result)
+            }
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json({errorMessage: "Could not create that user."})
+        });
+    }
+})
+router.get('', (req, res, next) => {
+    userDB.get().then((result) => {
+        if (!result.length) {
+            res.status(404).json({errorMessage: "There is no 'user' field in our database." })
+        } else {
+            res.status(200).json(result)
+        }
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).json({errorMessage: "Could not fetch users."})
+    });
+})
+router.get('/:id', (req,res, next) => {
+    userDB.getById(req.params.id).then((result) => {
+        if (!result) {
+            res.status(404).json({errorMessage: "There is no user by that id." })
+        } else {
+            res.status(200).json(result)
+        }
+    }).catch((err) => {
+        console.log(err)
+        res.status(500).json({errorMessage: "Could not fetch user."})
+    });
+})
+router.delete('/:id', (req, res, next) => {
+    userDB.remove(req.params.id).then((result) => {
+        if (result < 0) {
+            res.status(404).json({errorMessage: "Could not delete user."})
+        } else {
+            res.status(201).json(result)
+        }
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json({errorMessage: "Could not delete user."})
+    });
+})
+router.put('/:id', (req, res, next) => {
+    const { name } = req.body;
+    if (!name) {
+        res.status(404).json({errorMessage: "Please provide a name field to update."})
+    } else {
+        userDB.update( req.params.id ,{name}).then((result) => {
+            if (!result) {
+                res.status(404).json({errorMessage: "Could not update that user."})
+            } else {
+                res.status(201).json(result)
+            }
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json({errorMessage: "Could not update that user."})
+        });
+    }
+})
+
+
+
+module.exports = router;
