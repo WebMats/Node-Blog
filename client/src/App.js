ff9d3fc1eac818c2b0426b1c486fe9648c8f48bd
WebMats <lorlando127@gmail.com>
2019-02-13 18:33:47 -0500

    added some styling, and fixed some routing bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route, Link, withRouter } from 'react-router-dom'
 import PostsList from './components/PostsList/PostsList';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    userPosts: []
+    userPosts: [],
+    fetchedUsers: false
   }
   componentDidMount() {
     this.fetchUsers()
+    this.setState({fetchedUsers: true})
   }
   fetchUsers = () => {
     fetch('http://localhost:8000/users', {method: "GET"}).then(async (result) => {
@@ -23,9 +25,14 @@ class App extends Component {
   fetchPosts = (userId) => {
     fetch(`http://localhost:8000/users/posts/${userId}`, {method: "GET"}).then( async (result) => {
       const userPosts = await result.json();
+      if (userPosts.length === 0) {
+        this.setState({userPosts: "There are no posts for this user."})
+        return 
+      }
       this.setState({userPosts})
     }).catch((err) => {
-      
+      console.log(err)
+      throw err
     });
   }
   render() {
@@ -33,13 +40,17 @@ class App extends Component {
       <div className="App">
         <div className="users__container">
           <ul className="user__list">
-              {this.state.users.map(user => <li className="user__list-item" key={user.id}>{user.name} <span onClick={this.fetchPosts.bind(this, user.id)}><Link to={`/users/${user.id}`}>Show Posts</Link></span></li>)}
+              {this.state.users.map(user => <li className="user__list-item" key={user.id}>
+                                                {user.name} <button type="button" onClick={this.fetchPosts.bind(this, user.id)}>
+                                                              <Link to={`/users/${user.id}`}>Show Posts</Link>
+                                                            </button>
+                                            </li>)}
           </ul>
         </div>
-        <Route path="/users" render={() => <PostsList posts={this.state.userPosts} />} />
+        <Route path="/users/:id" render={() => <PostsList triggerFetch={this.fetchPosts} posts={this.state.userPosts} />} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- client/src/components/PostsList/PostsList.js
+++ client/src/components/PostsList/PostsList.js
@@ -1,14 +1,26 @@
 import React from 'react'
-
+import { withRouter} from 'react-router-dom'
 import './PostsList.css';
 
 
-const postsList = (props) => (
+const postsList = (props) => {
+    let user;
+    if (Array.isArray(props.posts) && props.posts.length < 1) {
+        props.triggerFetch(props.match.params.id)
+    } else {
+        user = props.posts[0].postedBy;
+    }
+    if (typeof props.posts === "string") {
+        return <p style={{fontSize:"3rem"}}>{props.posts}</p>
+    }
+    return (
     <div className="posts__container">
-          <ul className="posts__list">
+        <h3>Showing Posts by: {user}</h3>
+          <ol className="posts__list">
               {props.posts.map(post => <li className="posts__list-item" key={post.id}>{post.text}</li>)}
-          </ul>
+          </ol>
         </div>
-)
+    )
+}
 
-export default postsList
+export default withRouter(postsList)
